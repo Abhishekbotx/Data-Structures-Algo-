2c34917c2eac1994a06df82e682ce0ba5bf2882b
Abhishekbotx <abhishekbotx@gmail.com>
2025-05-18 02:43:14 +0530

    code push of longest consecutive problem with optimal approach

---
+++ longestConsecutiveSequence.js
@@ -0,0 +1,23 @@
+const arr = [100, 200, 1, 3, 2, 4];
+const longestConsecutiveSequence = (arr) => {
+    const n = arr.length;
+    arr.sort((a, b) => a - b); 
+    console.log('sorted array', arr);
+
+    let maxLength = 1;
+    let currentLength = 1;
+
+    for (let i = 0; i < n - 1; i++) { 
+        if (arr[i + 1] - arr[i] === 1) {
+            currentLength += 1;
+            maxLength = Math.max(maxLength, currentLength);
+        } else {
+            currentLength = 1;
+        }
+    }
+
+    return maxLength;
+};
+
+const ans = longestConsecutiveSequence(arr);
+console.log('answer>>', ans);
